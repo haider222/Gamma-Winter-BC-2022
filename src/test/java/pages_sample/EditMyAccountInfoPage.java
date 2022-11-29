1439a8336392562c08d27ba8f2d04ca5b493086d
mariannagare <garejeva.marianna@gmail.com>
2022-11-30 00:55:02 +0200

    defined elements for Edit My Account Onfo Page Object

--- src/test/java/pages_sample/EditMyAccountInfoPage.java
+++ src/test/java/pages_sample/EditMyAccountInfoPage.java
@@ -6,6 +6,23 @@ import org.openqa.selenium.support.How;
 
 public class EditMyAccountInfoPage {
 
+    @FindBy(how = How.ID, using = "#input-firstname")
+    private WebElement inputFirstName;
+
+    @FindBy(how = How.ID, using = "#input-lastname")
+    private WebElement inputLastName;
+
+    @FindBy(how = How.ID, using = "#input-email")
+    private WebElement inputEmail;
+
+    @FindBy(how = How.ID, using = "#input-telephone")
+    private WebElement inputTelephone;
+
+    @FindBy(how = How.CSS, using = "input[type=\"submit\"]")
+    private WebElement continueButton;
+
+    @FindBy(how = How.CSS, using = "//a[text()=\"Back\"]")
+    private WebElement backButton;
 
 
 }
