d5ee93065f85820a645038e717aec4b745b561b2
mariannagare <garejeva.marianna@gmail.com>
2022-12-01 12:02:27 +0200

    any valid changes

--- src/test/java/pages_sample/AddressListPage.java
+++ src/test/java/pages_sample/AddressListPage.java
@@ -1,6 +1,8 @@
 package pages_sample;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -19,6 +21,18 @@ public class AddressListPage {
 // To be used for Edit button  a.btn.btn-info
 // To be used for Delete button  a.btn.btn-danger
 
+
+//    public void clickAddressEditButton(String name, String job) {
+//        WebElement personInList = new RemoteWebElement() {
+//        };
+//        for (WebElement person : listOfPeople) {
+//            if (person.getText().contains(name) && person.getText().contains(job)) {
+//                personInList = person.findElement(By.cssSelector(".editbtn"));
+//            }
+//        }
+//        personInList.click();
+//
+//    }
 
 
 
