752faf572374ea3a1bc55e901a835ac0d0b5f38b
Julija Hmelnicka <julija.hmelnicka@gmail.com>
2022-12-01 15:16:40 +0200

    Last update Address pages

--- src/test/java/pages_sample/AddressListPage.java
+++ src/test/java/pages_sample/AddressListPage.java
@@ -21,6 +21,9 @@ public class AddressListPage {
     public void clickNewAddressButton() {
         newAddressButton.click();
     }
+    public void clickBackButton() {
+        backButton.click();
+    }
 
 
 
